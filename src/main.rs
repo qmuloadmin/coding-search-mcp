e342e89a9e25b86ccc2a6844cd393e60ade00e0d
Zach Bullough <zach.bullough@gmail.com>
2025-09-18 07:35:21 -0500

    add reddit support

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{fs::File, io::Read, str::FromStr};
 
 use anyhow::{Context, anyhow};
 use clap::Parser;
+use regex::Regex;
 use reqwest::header::{HeaderMap, USER_AGENT};
 use rmcp::{
     ErrorData, ServiceExt,
@@ -12,11 +13,11 @@ use rmcp::{
     transport::stdio,
 };
 use serde::{Deserialize, Serialize};
-use url::Url;
-use regex::Regex;
 use std::sync::LazyLock;
+use url::Url;
 
-static DOMXREF_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r#"\{\{domxref\("(?P<arg>[^"]+)"\)\}\}"#).unwrap());
+static DOMXREF_RE: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r#"\{\{domxref\("(?P<arg>[^"]+)"\)\}\}"#).unwrap());
 static TEMPLATE_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\{\{[^}]+\}\}").unwrap());
 
 #[derive(Parser)]
@@ -35,19 +36,25 @@ struct Config {
     #[arg(long, env)]
     /// The path where the MDN content github project lives, up to the leading "files" directory
     mdn_base_path: String,
+    #[arg(long, env)]
+    /// The reddit client id for reddit APIs
+    reddit_client_id: String,
+    #[arg(long, env)]
+    /// The reddit client secret for reddit APIs
+    reddit_client_secret: String,
 }
 
 #[derive(Deserialize, Default, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 struct GoogleSearchParams {
-    /// a list of words that _all_ must match _exactly_
+    /// a list of words that _all_ must match _exactly_ as a space separated string
     exact_terms: Option<String>,
-    /// a list of terms the _must not_ exist in the results
+    /// a list of terms the _must not_ exist in the results as a space separated string
     exclude_terms: Option<String>,
     // max len is 100 results, so no possible need for more than 255
     /// when viewing multiple pages, the offset, or index of the first result
     start: Option<u8>,
-    /// the required query itself, the search term(s)
+    /// the required query itself, the search term(s), as a string. E.g. "typescript enum to string method"
     query: String,
 }
 
@@ -70,6 +77,7 @@ async fn main() -> Result<(), anyhow::Error> {
 
 struct Tools {
     config: Config,
+    reddit_client: roux::Reddit,
     tool_router: ToolRouter<Self>,
 }
 
@@ -88,17 +96,24 @@ impl rmcp::ServerHandler for Tools {
 impl Tools {
     fn new(config: Config) -> Self {
         Self {
-            config,
             tool_router: Self::tool_router(),
+            reddit_client: roux::Reddit::new(
+                "linux:nimbus:v0.1.0 (by /u/Keozon)",
+                &config.reddit_client_id,
+                &config.reddit_client_secret,
+            ),
+            config,
         }
     }
 
-    #[tool(description = "Search a subset of sites in Google for a list of matching web pages")]
+    #[tool(
+        description = "Search a subset of sites in Google for a list of matching web pages with snippets of information"
+    )]
     async fn query_google_search(
         &self,
         params: Parameters<GoogleSearchParams>,
     ) -> Result<CallToolResult, ErrorData> {
-        let client = self.get_client();
+        let client = self.get_http_client();
         let base_route = "https://customsearch.googleapis.com/customsearch/v1?";
         let mut builder = client.get(base_route);
         if let Some(exact_terms) = params.0.exact_terms.as_ref() {
@@ -126,7 +141,7 @@ impl Tools {
         Ok(CallToolResult::success(vec![Content::text(json)]))
     }
 
-    fn get_client(&self) -> reqwest::Client {
+    fn get_http_client(&self) -> reqwest::Client {
         let mut headers = HeaderMap::new();
         headers.insert(
             USER_AGENT,
@@ -150,12 +165,59 @@ impl Tools {
         let mut contents = String::new();
         file.read_to_string(&mut contents)
             .context("unable to read mdn page file")?;
-		contents = DOMXREF_RE.replace_all(&contents, "`$arg`").to_string();
+        contents = DOMXREF_RE.replace_all(&contents, "`$arg`").to_string();
         Ok(TEMPLATE_RE.replace_all(&contents, "").to_string())
     }
 
+    async fn fetch_reddit_page(&self, submission_id: &str) -> Result<Vec<String>, anyhow::Error> {
+        let session = self
+            .reddit_client
+            .clone()
+            .login()
+            .await
+            .context("logging into reddit")?;
+        let mut submission = session
+            .get_submissions(submission_id)
+            .await
+            .context("getting submission")?;
+		let submission = submission.data.children.swap_remove(0);
+        let title = submission.data.title;
+        let contents = submission.data.selftext;
+        let likes = submission.data.score;
+        let subreddit = submission.data.subreddit;
+        let mut thread: Vec<String> = Vec::new();
+        let sub = format!(
+            "<h1>{}: {}</h1><p>Score/Likes: {}</p><p>{}</p>",
+            subreddit, title, likes, contents
+        );
+        thread.push(sub);
+        let comment_client = roux::Subreddit::new_oauth(&subreddit, &session.client);
+        let comments = comment_client
+            .article_comments(submission_id, Some(2), Some(3))
+            .await
+            .context("fetching submission comments")?;
+        // TODO make sure the snippet returned from google search is in returned comments
+		// TODO build graph of replies
+        for comment in comments.data.children.into_iter() {
+            if let Some(body) = comment.data.body {
+                let user = comment.data.author.unwrap_or("unknown redditor".into());
+                let id = comment.data.name.unwrap(); // How could this be null?
+				let response_to = if let Some(parent) = comment.data.parent_id {
+					format!(" In response to: {}", parent)
+				} else {
+					format!("")
+				};
+                thread.push(format!(
+                    "<h1>Comment: {} from {}{}</h1><p>{}</p>",
+                    id, user, response_to, body
+                ))
+            }
+        }
+        Ok(thread)
+    }
+
     async fn fetch_so_page(&self, question_id: &str) -> Result<Vec<String>, anyhow::Error> {
-        let client = self.get_client();
+        let client = self.get_http_client();
         let so_questions_path = format!(
             "{}/questions/{}",
             self.config.stack_overflow_api_prefix, question_id
@@ -200,7 +262,7 @@ impl Tools {
                     is_accepted,
                     question_id: _,
                 }) => format!(
-                    "<h1>{} answer with {} votes</h1><pre>{}</pre>",
+                    "<h1>{} answer with {} votes</h1><p>{}</p>",
                     if is_accepted {
                         "Accepted"
                     } else {
@@ -212,18 +274,18 @@ impl Tools {
                 StackExchangeItem::Question(StackExchangeQuestionFields {
                     common,
                     tags: _,
-                    is_answered,
+                    is_answered: _,
                     view_count: _,
                     answer_count: _,
                     link: _,
                     title,
-                }) => format!("<h1>{}</h1><pre>{}</pre>", title, common.body),
+                }) => format!("<h1>{}</h1><p>{}</p>", title, common.body),
             })
             .collect())
     }
 
     #[tool(
-        description = "Retrieve the primary contents of a supported webpage, as reterned in a link in a previous search"
+        description = "Retrieve the primary contents of a supported webpage, as reterned in a link in a previous search if the snippet from search is insufficient."
     )]
     async fn fetch_result_page(
         &self,
@@ -256,6 +318,20 @@ impl Tools {
                             .await
                             .map_err(|err| ErrorData::internal_error(format!("{}", err), None))?,
                     )])),
+					"www.reddit.com" => {
+						let submissision_id = parsed.path_segments().unwrap().nth(3).ok_or(ErrorData::invalid_params(
+							"invalid reddit URL: missing comment/submission id in path",
+							None
+						))?;
+						Ok(CallToolResult::success(
+                            self.fetch_reddit_page(submissision_id)
+                                .await
+                                .map_err(|err| ErrorData::internal_error(format!("{}", err), None))?
+                                .into_iter()
+                                .map(|comment| Content::text(comment))
+                                .collect(),
+                        ))
+					}
                     _ => Err(ErrorData::invalid_params(
                         format!(
                             "invalid host: {}. Must be from provided search results",
@@ -276,6 +352,7 @@ impl Tools {
 #[derive(Deserialize, Serialize)]
 struct GoogleSearchResults {
     items: Vec<GoogleSearchResult>,
+    queries: GoogleSearchQueryData,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -287,6 +364,21 @@ struct GoogleSearchResult {
 }
 
 #[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct GoogleSearchQueryData {
+    next_page: GoogleSearchPage,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct GoogleSearchPage {
+    title: String,
+    total_results: String,
+    count: usize,
+    start_index: usize,
+}
+
+#[derive(Deserialize, Serialize)]
 #[serde(untagged)]
 enum PageMap {
     MDN(MDNPageMap),
@@ -294,9 +386,12 @@ enum PageMap {
 }
 
 #[derive(Deserialize, Serialize)]
+// Encapsulates general, not-customized responses. Works for MDN, Reddit, etc.
 struct MDNMeta {
     #[serde(rename = "og:description")]
     description: String,
+    #[serde(rename = "og:title")]
+    title: String,
 }
 
 #[derive(Deserialize, Serialize)]
